f147e110bc8280179280b5167d63f65e197e6400
Tyler Rider <40363454+TroubleShoot175@users.noreply.github.com>
2023-05-05 21:14:01 -0500

    Completed First Next.js Application

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import Link from 'next/link';
+import Script from 'next/script';
+
+export default function FirstPost() {
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2><Link href='/'>Back to home</Link></h2>
+        </>
+    );
+}
